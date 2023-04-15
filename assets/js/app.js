aeeaf9c9f6f86695abf90058036fc71734cd86d8
Victor Akindele <105135998+hurlavic@users.noreply.github.com>
2023-04-15 01:49:59 +0100

    added styling to page

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+let dayInput = document.querySelector("#day");
+let monthInput = document.querySelector("#month");
+let yearInput = document.querySelector("#year");
+let form = document.querySelector(".form");
+let errorContent = document.querySelector(".error");
+// let button = document.querySelector("button")
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const day = dayInput.value;
+  const month = monthInput.value;
+  const year = yearInput.value;
+
+  if (day === "" || month === "" || year === "") {
+    errorContent.textContent = "Field(s) can't be blank";
+    dayInput.style.border = "1px solid red";
+    monthInput.style.border = "1px solid red";
+    yearInput.style.border = "1px solid red";
+
+    if (day) {
+      dayInput.style.border = "";
+    }
+    if (month) {
+      monthInput.style.border = "";
+    }
+    if (year) {
+      yearInput.style.border = "";
+    }
+  }
+});
